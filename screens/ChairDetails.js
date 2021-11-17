d87e401d6d2e42891c80e2fa2d4100e61ddc4ddb
emmanuel <90016711+cejaiy@users.noreply.github.com>
2021-11-17 15:43:36 +0000

    Home Screen Fixed

--- screens/ChairDetails.js
+++ screens/ChairDetails.js
@@ -16,6 +16,7 @@ const ChairDetails = ({ navigation, route }) => {
 
     const scrollX = new Animated.Value(0);
     const [chairbrands, setChairbrands] = React.useState(null);
+    const [orderItems, setOrderItems] = React.useState([]);
 
     React.useEffect(() => {
         let { item } = route.params;
@@ -23,4 +24,69 @@ const ChairDetails = ({ navigation, route }) => {
         setChairbrands(item)
     })
 
+    function renderHeader() {
+        return (
+            <View style={{ flexDirection: 'row' }}>
+                <TouchableOpacity
+                    style={{
+                        width: 50,
+                        paddingLeft: SIZES.padding * 2,
+                        justifyContent: 'center'
+                    }}
+                    onPress={() => navigation.goBack()}
+                >
+                    <Image
+                        source={icons.back}
+                        resizeMode="contain"
+                        style={{
+                            width: 30,
+                            height: 30
+                        }}
+                    />
+                </TouchableOpacity>
+
+                {/* Chair brands Name Section */}
+                <View
+                    style={{
+                        flex: 1,
+                        alignItems: 'center',
+                        justifyContent: 'center'
+                    }}
+                >
+                    <View
+                        style={{
+                            height: 50,
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                            paddingHorizontal: SIZES.padding * 3,
+                            borderRadius: SIZES.radius,
+                            backgroundColor: COLORS.lightGray3
+                        }}
+                    >
+                        <Text style={{ ...FONTS.h3 }}>{chairbrands?.name}</Text>
+                    </View>
+                </View>
+
+                <TouchableOpacity
+                    style={{
+                        width: 50,
+                        paddingRight: SIZES.padding * 2,
+                        justifyContent: 'center'
+                    }}
+                >
+                    <Image
+                        source={icons.list}
+                        resizeMode="contain"
+                        style={{
+                            width: 30,
+                            height: 30
+                        }}
+                    />
+                </TouchableOpacity>
+            </View>
+        )
+    }
+
 }
+
+export default ChairDetails;
